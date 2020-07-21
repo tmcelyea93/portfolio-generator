1b44de783b519aa2cbd0165be1c984f6eef48ab1
tmcelyea93 <tarahmcelyea@gmail.com>
2020-07-21 13:43:49 -0500

    added HTML function into own page

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const name = profileDataArgs[0];
+const github = profileDataArgs[1];
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 
 
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
 
-
-
-
-
-
-
-
-
-
-
-
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-    console.log('================');
-
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
-  
-printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
